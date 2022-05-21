24b09fab8cea9494042edf75cda26fcea47fbf8c
[4기-1반] 김채정 <whenucan35@gmail.com>
2022-05-22 00:03:00 +0900

    [Add] Auth.js login form

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,30 @@
+import { useState } from 'react'
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
 
-export default Auth;
+    const onChange = event => {
+        console.log(event.target.name)
+    }
+
+    const onSubmit = event => {
+        event.preventDefault()
+    }
+
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input type="email" name="email" placeholder="Email" required value={email} onChange={onChange} />
+                <input type="password" name="password" placeholder="Password" required value={password} onChange={onChange} />
+                <input type="submit" value="Log In" />
+            </form>
+            <div>
+                <button>Continue with Google</button>
+                <button>Continue with Github</button>
+            </div>
+        </div>
+    )
+}
+
+export default Auth
