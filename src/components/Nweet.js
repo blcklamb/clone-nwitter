812e9ec7fe6cfd6ead9686aaeb23a6e0843b4ef0
blcklamb <whenucan35@gmail.com>
2022-05-29 15:20:37 +0900

    [Feat] nweet delete

---
+++ src/components/Nweet.js
@@ -0,0 +1,26 @@
+import { dbService } from 'fbase'
+
+const Nweet = ({ nweetObj, isOwner }) => {
+  const onDeleteClick = async () => {
+    const ok = window.confirm('Do you want to delete this nweet?')
+    console.log(ok)
+    if (ok) {
+      console.log(nweetObj.id)
+      const data = await dbService.doc(`nweets/${nweetObj.id}`).delete()
+      console.log(data)
+    }
+  }
+  return (
+    <div>
+      <h4>{nweetObj.text}</h4>
+      {isOwner && (
+        <>
+          <button onClick={onDeleteClick}>Delete Nweet</button>
+          <button>Edit Nweet</button>
+        </>
+      )}
+    </div>
+  )
+}
+
+export default Nweet
