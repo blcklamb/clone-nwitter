bf805d4eab9fb98b8a59aeb4986874c2dd46c8f4
blcklamb <whenucan35@gmail.com>
2022-06-01 01:33:25 +0900

    [Feat] My Profile show posting

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,16 +1,45 @@
-import { authService } from 'fbase'
+import { authService, dbService } from 'fbase'
+import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import Nweet from 'components/Nweet'
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
   const navigate = useNavigate()
+  const [nweets, setNweets] = useState([])
 
   const onLogOutClick = () => {
     authService.signOut()
     navigate('/')
   }
+
+  const getMyNweets = async () => {
+    const nweets = await dbService
+      .collection('nweets')
+      .where('creatorId', '==', userObj.uid)
+      .orderBy('createdAt', 'asc')
+      .get()
+    // .onSnapshot(snapshot => {
+    //   const newArray = snapshot.docs.map(document => ({
+    //     id: document.id,
+    //     ...document.data(),
+    //   }))
+    //   setNweets(newArray)
+    // })
+    const newArray = await nweets.docs.map(doc => doc.data())
+    setNweets(newArray)
+  }
+
+  useEffect(() => {
+    getMyNweets()
+  }, [])
   return (
     <>
       <button onClick={onLogOutClick}>Log Out</button>
+      <div>
+        {nweets.map(nweet => {
+          return <Nweet key={nweet.id} nweetObj={nweet} />
+        })}
+      </div>
     </>
   )
 }
